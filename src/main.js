da84794e5772f33568cd77f27a0da8412a4b38da
dracero <dracero>
2023-08-30 20:39:32 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import { World } from './World/World.js';
+
+
+function main() {
+    // Get a reference to the container element
+    const container = document.querySelector('#scene-container');
+  
+    // 1. Create an instance of the World app
+    const world = new World(container);
+    // We can access member variables from the instance
+    console.log(world.camera);
+    console.log(world.renderer);
+    console.log(world.scene);
+  
+    // 2. Render the scene
+    world.render();
+  }
+
+  main();
